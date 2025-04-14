c0c5ff6231af4248be3e398bffceea18a93bde65
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-04-14 20:44:23 +0530

    Solved medium level questions on 2-D array, matrix multiplication and optimized search of an element.

---
+++ DSA_Revision/src/twoDimensionalArrays/MatrixMultiplication.java
@@ -0,0 +1,33 @@
+package twoDimensionalArrays;
+
+public class MatrixMultiplication {
+    public static void print(int[][] arr) {
+        for(int i = 0;  i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 1, 1}, {2, 1, 3, 2}}; // 2*4
+        int[][] b = {{1, 2, 1}, {2, 1, 2}, {3, 2, 2}, {1, 2, 2}}; // 4*3
+
+        if(a[0].length != b.length) {
+            System.out.println("Multiplication not possible");
+            return;
+        }
+        else {
+            int[][] ans = new int[a.length][b[0].length];
+            for(int i = 0; i < ans.length; i++) {
+                for(int j = 0; j < ans[i].length; j++) {
+                    for(int k = 0; k < b.length; k++) { // or a[0].length
+                        ans[i][j] += a[i][k] * b[k][j];
+                    }
+                    System.out.print(ans[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}

f4e73113cd5b9c780be63f510c7746f3b8a97b76
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-05-13 20:07:02 +0530

    Solved some questions on Strings.

---
+++ DSA_Revision/src/stringAndStringBuilder/StringCompression.java
@@ -0,0 +1,34 @@
+package stringAndStringBuilder;
+
+public class StringCompression {
+        public static int compress(char[] arr) {
+            int i = 0, j = 0, n = arr.length, count = 0;
+
+            StringBuilder sb = new StringBuilder();
+            while(j < n) {
+                sb.append(arr[i]);
+                while(j < n && arr[i] == arr[j]) {
+                    count++;
+                    j++;
+                }
+                if(count > 1) {
+                    sb.append(count);
+                }
+                count = 0;
+                i = j;
+                j = i;
+            }
+            for(int k = 0; k < sb.length(); k++) {
+                arr[k] = sb.charAt(k); // compressed the original array
+                System.out.print(sb.charAt(k) + " ");
+            }
+            System.out.println();
+            return sb.length(); // return the compressed length
+        }
+
+    public static void main(String[] args) {
+        char[] arr = {'a', 'b', 'b', 'b', 'c', 'd', 'e', 'e', 'e', 'e', 'e'} ;
+        int ans = compress(arr);
+        System.out.println(ans);
+    }
+}

518f005afedc3a7e3cbe3bd304d829d52cdd6451
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-07-07 00:04:21 +0530

    Revisied some topics.

--- DSA_Revision/src/sorting/insertion_sort/InsertionSort.java
+++ DSA_Revision/src/sorting/insertion_sort/InsertionSort.java
@@ -17,20 +17,20 @@ public class InsertionSort {
         int[] arr = {5, 2, 6, 1, 9, 1, 46, 24, 74, 26, 43, 73, 75, 23, 68};
         print(arr);
 
-//        for(int i = 1; i < arr.length; i++) {
-//            for(int j = i; j >= 1 && arr[j] < arr[j - 1]; j--) {
-//                swap(arr, j, j-1);
-//            }
-//        }
+        for(int i = 1; i < arr.length; i++) {
+            for(int j = i; j >= 1 && arr[j] < arr[j - 1]; j--) {
+                swap(arr, j, j-1);
+            }
+        }
         print(arr);
 
         // Insertion sort using while loop
-        for(int i = 1; i < arr.length; i++) {
-            int j = i;
-            while(j >=1 && arr[j] < arr[j - 1]) {
-                swap(arr, j, j-1);
-                j--;
-            }
-        }
+//        for(int i = 1; i < arr.length; i++) {
+//            int j = i;
+//            while(j >=1 && arr[j] < arr[j - 1]) {
+//                swap(arr, j, j-1);
+//                j--;
+//            }
+//        }
     }
 }

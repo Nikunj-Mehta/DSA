a393238d436a976e5c1698cc4b309881fcf7579b
Nikunj-Mehta <mehtanikunj11@gmail.com>
2026-01-31 22:07:51 +0530

    Completed the implemented of Linked List with functions like insertAtIndex, getElementAtIdex, deleteHead, deleteAtIndex. Along with this completed a module, Module 1 of Linked List

--- DSA_Revision/src/linkedList/ImplementationOfLL.java
+++ DSA_Revision/src/linkedList/ImplementationOfLL.java
@@ -29,6 +29,44 @@ class SLL { // User defined Data Structure
         size++;
     }
 
+    public void set(int idx, int value) throws Error{
+        if(idx > size || idx < 0){
+            throw new Error("Invalid index");
+        }
+        if(idx == size-1) {
+            tail.value = value;
+            return;
+        }
+        Node temp = head;
+        for(int i = 0; i < idx; i++) {
+            temp = temp.next;
+        }
+        temp.value = value;
+    }
+
+    public void insert(int idx, int value) {
+        if(idx > size || idx < 0){
+            System.out.println("Error: Invalid index");
+            return;
+        }
+        if(idx == 0) {
+            insertAtHead(value);
+            return;
+        }
+        if(idx == size) {
+            insertAtEnd(value);
+            return;
+        }
+        Node newNode = new Node(value);
+        Node temp = head;
+        for(int i = 0; i < idx-1; i++) {
+            temp = temp.next; // temp will stop on Node just before idx.
+        }
+        newNode.next = temp.next; // new node will point where temp is pointing that is to the next node
+        temp.next = newNode; // Now temp will point to new node and new node is inserted in between successfully
+        size++;
+    }
+
     public void insertAtEnd(int value) { // TC = O(1)
         Node temp = new Node(value);
         if(head == null) head = tail = temp; // Linked List is empty
@@ -37,6 +75,50 @@ class SLL { // User defined Data Structure
             tail = temp;
         }
         size++;
+    }
+
+    public void getElement(int idx) {
+        if(idx >= size || idx < 0){
+            System.out.println("Error: Invalid index");
+            return;
+        }
+        if(idx == size - 1) {
+            System.out.println(tail.value);
+            return;
+        }
+        Node temp = head;
+        for(int i = 0; i < idx; i++) {
+            temp = temp.next;
+        }
+        System.out.println(temp.value);
+    }
+
+    public void deleteAtHead() throws Error{
+        if(head == null) {
+            throw new Error("Linked List is Empty");
+        }
+        head = head.next;
+        size--;
+    }
+
+    public void deleteAtIndex(int idx) throws Error{
+        if(idx >= size || idx < 0) {
+            throw new Error("Invalid Index");
+        }
+
+        if(idx == 0) {
+            deleteAtHead();
+            return;
+        }
+        Node temp = head;
+        for(int i = 0; i < idx-1; i++) { // Temp reaches at index - 1
+            temp = temp.next;
+        }
+        temp.next = temp.next.next; // That is temp.next.next; skipping the middle node. Line of deletion
+
+        if(idx == size - 1) tail = temp;
+
+        size--;
     }
 }
 
@@ -60,8 +142,57 @@ public class ImplementationOfLL {
         list.insertAtHead(5);
         list.size();
         list.display();
+
         list.insertAtHead(1);
         list.size();
         list.display();
+
+        list.insert(0, -1);
+        list.size();
+        list.display();
+
+        list.insert(3, 8);
+        list.size();
+        list.display();
+
+        list.insert(9, 80);
+        list.size();
+        list.display();
+
+        list.insert(11, 90);
+        list.size();
+        list.display();
+
+        list.insert(12, 100);
+        list.size();
+        list.display();
+
+        list.getElement(4);
+
+        list.set(4, 9);
+        list.display();
+//        list.set(-1, 59); // User defined error
+
+        list.set(12, 99);
+        list.display();
+        list.size();
+
+        list.deleteAtIndex(4);
+        list.display();
+        list.size();
+
+        System.out.println(list.tail.value);
+        list.deleteAtIndex(11);
+        System.out.println(list.tail.value);
+        list.display();
+        list.size();
+
+
+        list.deleteAtIndex(0);
+        list.display();
+        list.size();
+
+        list.deleteAtIndex(-1);
+        list.deleteAtIndex(12);
     }
 }
